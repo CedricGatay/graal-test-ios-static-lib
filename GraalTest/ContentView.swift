8382321430b6bebdc60217fb18dedbe18ca07f8e
Cedric Gatay <c.gatay@code-troopers.com>
2019-10-25 11:31:19 +0200

    Initial Commit

---
+++ GraalTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraalTest
+//
+//  Created by Cedric Gatay on 25/10/2019.
+//  Copyright © 2019 Code-Troopers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
